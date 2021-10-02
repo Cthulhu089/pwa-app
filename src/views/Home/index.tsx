c0b87fb314cc42bd13f8576ef606ddd4e68136b3
Marco Polo Vazquez Gutierrez <marco.vazquez@agilethought.com>
2021-10-02 17:48:04 -0500

    Set the snackbar and the reload functionality

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Marco test <code>src/App.tsx</code> and save to reload.
+          PWA app <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
