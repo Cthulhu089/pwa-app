793dd15fa1009292cf7f34b0fc8e497f190c7a4b
Marco Polo Vazquez Gutierrez <marco.vazquez@agilethought.com>
2021-10-02 11:22:24 -0500

    Fix index.html

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { initializeApp } from "firebase/app";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-// Your web app's Firebase configuration
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA0hGXXO5c3QuVH7tumL6wG0AgcFsPhgAU",
-  authDomain: "pwa-app-bad34.firebaseapp.com",
-  projectId: "pwa-app-bad34",
-  storageBucket: "pwa-app-bad34.appspot.com",
-  messagingSenderId: "1053967934127",
-  appId: "1:1053967934127:web:6f323cb2964e3d5747ea5a",
-};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,14 +12,10 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
