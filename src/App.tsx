f601a888e31598196d8c64d74de253982aa4ed19
Marco Polo Vazquez Gutierrez <marco.vazquez@agilethought.com>
2021-10-02 17:23:58 -0500

    Set the snackbar and the reload functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useCallback, useEffect, useState } from "react";
+import Home from "./views/Home";
+import SnackBar from "my-react-snackbar";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 function App() {
+  const [showSnackBar, setShowSnackbar] = useState<boolean>(false);
+  const [registration, setRegistration] = useState<ServiceWorkerRegistration>();
+
+  const onSuccess = useCallback(() => {}, []);
+  const onUpdate = useCallback((registration: ServiceWorkerRegistration) => {
+    if (!!registration && !!registration.waiting) {
+      setShowSnackbar(true);
+      setRegistration(registration);
+    }
+  }, []);
+
+  const handleOnUpdate = useCallback(async () => {
+    if (!!registration && !!registration.waiting) {
+      await registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      window.location.reload();
+    }
+  }, [registration]);
+
+  useEffect(() => {
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://cra.link/PWA
+    serviceWorkerRegistration.register({
+      onSuccess,
+      onUpdate,
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Marco <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SnackBar
+        open={showSnackBar}
+        closeOnClick={false}
+        message={"New version available"}
+        position="bottom-center"
+        type="info"
+        yesLabel="Update"
+        onYes={handleOnUpdate}
+      />
+      <Home />
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
@@ -11,21 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-const onSuccess = () => {};
-const onUpdate = (registration: ServiceWorkerRegistration) => {
-  if (!!registration && !!registration.waiting) {
-    registration.waiting.postMessage({ type: "SKIP_WAITING" });
-  }
-};
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({
-  onSuccess,
-  onUpdate,
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
